53a0d885d4d316deee788dfdb304a89a9e4c8f8c
Parth Bhavsar <parth.12012004@gmail.com>
2024-07-14 09:35:54 +0530

    nav updated

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './nav.css';
 import '../../css/var.css';
+import Logo from './../../assets/fav-icon.png';
 // import { Link } from 'react-router-dom';
 
 function Nav() {
@@ -9,8 +10,8 @@ function Nav() {
       <nav>
         <ul>
           <div className="left">
-            {/* <li><Link to="#"><i class="zmdi zmdi-home"></i><img src="" alt="logo" /></Link></li> */}
-            <li><i class="zmdi zmdi-home"></i><img src="" alt="logo" /></li>
+            {/* <li><Link to="#"><img src={Logo} alt="logo" className="logo"/>OdooShelf</Link></li> */}
+            <li><img src={Logo} alt="logo" className="logo"/>OdooShelf</li>
           </div>
           <div className="center">
             {/* <li><Link to="#"><i class="zmdi zmdi-library"></i> Books</Link></li> */}
