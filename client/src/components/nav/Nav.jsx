1cfbba779265e7ce66f813c35b4a8cb51eab10c8
Parth Bhavsar <parth.12012004@gmail.com>
2024-07-14 09:18:06 +0530

    Link added in Nav

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './nav.css';
 import '../../css/var.css';
+// import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
@@ -8,11 +9,14 @@ function Nav() {
       <nav>
         <ul>
           <div className="left">
-            <li><i class="zmdi zmdi-home"></i><img src="https://www.google.com/imgres?q=library%20logo&imgurl=https%3A%2F%2Fpng.pngtree.com%2Fpng-clipart%2F20200701%2Foriginal%2Fpngtree-hand-drawn-cartoon-polygon-library-bookshelf-illustration-png-image_5357151.jpg&imgrefurl=https%3A%2F%2Fpngtree.com%2Fso%2Flibrary-logo&docid=rbz_vd8Qa-bn9M&tbnid=MtvvIu6OddhHrM&vet=12ahUKEwj35qTDy6WHAxXmXmwGHQthCoQQM3oECH4QAA..i&w=1200&h=1200&hcb=2&ved=2ahUKEwj35qTDy6WHAxXmXmwGHQthCoQQM3oECH4QAA" alt="logo" /></li>
+            {/* <li><Link to="#"><i class="zmdi zmdi-home"></i><img src="" alt="logo" /></Link></li> */}
+            <li><i class="zmdi zmdi-home"></i><img src="" alt="logo" /></li>
           </div>
           <div className="center">
-            <li><i class="zmdi zmdi-library"></i>Books</li>
-            <li><i class="zmdi zmdi-notifications"></i>Notifications</li>
+            {/* <li><Link to="#"><i class="zmdi zmdi-library"></i> Books</Link></li> */}
+            <li><i class="zmdi zmdi-library"></i> Books</li>
+            {/* <li><Link to="#"><i class="zmdi zmdi-notifications"></i> Notifications</Link></li> */}
+            <li><i class="zmdi zmdi-notifications"></i> Notifications</li>
           </div>
           <div className="right">
             <li><button className='btnLogout'><i class="zmdi zmdi-power"></i>Log Out</button></li>
