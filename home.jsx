4d0e55aff255b5bf6c564d488b6a1cffeb1a5265
Kishan Bhambhariya <129962614+Kishan-Bhambhariya@users.noreply.github.com>
2024-07-14 12:31:50 +0530

    home page doneeeeee

--- home.jsx
+++ home.jsx
@@ -1,14 +1,18 @@
-import "./Home.css"
+import "./home.css";
+import bookimg from "./book.png"
 
 const Home = () => {
   return (
-    <div className="home">
-     <img src="../../photos/home.jpg" alt="" width="100%" height="100%" />
+   
+     <div className="home">
       <div className="data">
-        <span> The library is the people's university. </span>
-      <p>Welcome to a world of knowledge at your fingertips! Our library management system empowers libraries to streamline operations, enhance accessibility, and foster a love of learning for everyone.</p>
+        <span> The library is the people&apos;s university. </span>
+        <p>Welcome to a world of knowledge at your fingertips! Our library management system empowers libraries to streamline operations, enhance accessibility, and foster a love of learning for everyone.</p>
+        <button className="getStarted">Get Started<i className="zmdi zmdi-caret-right-circle"></i></button>
+      </div>
       
-    </div>
+      <div className="img"><img src={bookimg} alt="" />
+      </div>
     </div>
   )
 }
