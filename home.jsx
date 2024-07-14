78d8800f88cd7c671db3022c203dd96b57e9f78c
Kishan Bhambhariya <129962614+Kishan-Bhambhariya@users.noreply.github.com>
2024-07-14 10:18:40 +0530

    homepage done

---
+++ home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className="home">
+     <img src="../../photos/home.jpg" alt="" width="100%" height="100%" />
+      <div className="data">
+        <span> The library is the people's university. </span>
+      <p>Welcome to a world of knowledge at your fingertips! Our library management system empowers libraries to streamline operations, enhance accessibility, and foster a love of learning for everyone.</p>
+      
+    </div>
+    </div>
+  )
+}
+
+export default Home;
